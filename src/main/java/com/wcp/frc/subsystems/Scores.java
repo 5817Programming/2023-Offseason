d6e48b6d4a69a01c3353a323e65b4f47a2431f80
5817Programming <132308753+5817Programming@users.noreply.github.com>
2023-05-02 09:30:23 -0700

    Elvator rework

--- src/main/java/com/wcp/frc/subsystems/Scores.java
+++ src/main/java/com/wcp/frc/subsystems/Scores.java
@@ -46,167 +46,139 @@ public class Scores extends SubsystemBase {
   public enum State{
     HIGH, MID, LOW, ZERO, CHUTE, HUMAN;
   }
-  public void setState(State newState){
+  public void setHeight(State newState){
       currentState = newState;
   }
 
-  public void score(boolean coDriverA, boolean coDriverB, boolean coDriverX, boolean coDriverY, 
+  public void setHeight(boolean coDriverA, boolean coDriverB, boolean coDriverX, boolean coDriverY, 
   boolean coDriverBackButton, boolean coDriverLeftStickDown, boolean coDriverLeftBumperTAP, boolean Cube) {
     
     if (coDriverA) {
-      height = Constants.ElevatorConstants.LOW_CONE;
+      currentState = State.LOW;
     } else if (coDriverB) {
-      height = Constants.ElevatorConstants.MID_CONE;
+      currentState = State.LOW;
     } else if (coDriverX) {
-      height = Constants.ElevatorConstants.HOOMAN_CONE;
+      currentState = State.HOOMAN;
     } else if (coDriverY) {
-      height = Constants.ElevatorConstants.HIGH_CONE;
+      currentState = State.HIGH;
     } else if (coDriverBackButton) {
-      height = Constants.ElevatorConstants.ZERO;
+      currentState = State.CHUTE;
     } else if (coDriverLeftStickDown) {
-      height = Constants.ElevatorConstants.HOLD;
-    } else if (coDriverLeftBumperTAP) {
-
+      currentState = State.ZERO;
     }
     this.Cube = Cube;
+  }
+
+  public static void selectPreset(){
+    if(curentState == State.HIGH&&!CUBE){
+      
+    }
+    if(curentState == State.MID&&!CUBE){
+      
+    }
+    if(curentState == State.LOW&&!CUBE){
+      
+    }
+    if(curentState == State.HIGH&&CUBE){
+      
+    }
+    if(curentState == State.MID&&CUBE){
+      
+    }
+    if(curentState == State.LOW&&CUBE){
+      
+    }
+    if(currentState == State.CHUTE){
+
+    }
+    if(currentState == )
+  }
+  public static void setElevator(State state){
+    currentState = state;
+    selectPreset();
+
+  }
+  public static void goToPreset(){
 
   }
 
-  public void scoring() {
-
-    if (height == Constants.ElevatorConstants.LOW_CONE&&Cube) {
-      scoreLowCube();
-
-    }
-    if (height == Constants.ElevatorConstants.MID_CONE&&Cube) {
-      scoreMidCube();
-     
-    }
-    if (height == Constants.ElevatorConstants.HIGH_CONE&&Cube) {
-      scoreHighCube();
-    }
-    if (height == Constants.ElevatorConstants.HOOMAN_CONE&&Cube ) {
-      HOOMAN_CUBE();
-    }
-    
-    if (height == Constants.ElevatorConstants.LOW_CONE&&Cube == false) {
-      scoreLowCone();
-
-    }
-    if (height == Constants.ElevatorConstants.MID_CONE&&Cube == false) {
-      scoreMidCone();
-     
-    }
-    if (height == Constants.ElevatorConstants.HIGH_CONE&&Cube == false) {
-      scoreHighCone();
-    }
-    if (height == Constants.ElevatorConstants.HOOMAN_CONE&&Cube == false) {
-      hooman_CONE();
-    }
-    if (height == Constants.ElevatorConstants.ZERO) {
-      zero();
-    }
-    if (height == Constants.ElevatorConstants.HOLD) {
-      hold();
-    }
-
-    if (height == Constants.ElevatorConstants.PICKUP) {
-      pickup();
-    }
-  }
-
-  public void hold() {
-    sideElevator.elevator(0+sideElvator);
+ public void hold() {
+    sideElevator.elevator(0);
     elevator.elevator(Constants.ElevatorConstants.MAX_DOWN);
-    arm.setMotionMagic(Constants.ArmConstants.HOLD+ armOffset);
-     armOffset=0;
-     sideElvator=0;
+    armDesired = (Constants.ArmConstants.HOLD- armOffset);
+    // armOffset=0;
   }
 
-  public void hooman_CONE() {
-    sideElevator.elevator(Constants.SideElevatorConstants.HOOMAN+sideElvator);// go to set offset based off vision
-    elevator.elevator(Constants.ElevatorConstants.HOOMAN_CONE);
-    if (elevator.isFinisheddown(20000)){
-      arm.setMotionMagic(Constants.ArmConstants.HOOMAN+armOffset);
-      }
-   
-
-  }
-  public void HOOMAN_CUBE() {
-    sideElevator.elevator(Constants.SideElevatorConstants.HOOMAN+sideElvator);// go to set offset based off vision
-    elevator.elevator(Constants.ElevatorConstants.HOOMAN_CUBE);
-    if (elevator.isFinisheddown(20000)){
-      arm.setMotionMagic(Constants.ArmConstants.HOOMAN+armOffset);
-      }
+  public void hooman() {
+    sideElevatorDesired = (Constants.SideElevatorConstants.HOOMAN);// go to set offset based off vision
+    elevatorDesired = (Constants.ElevatorConstants.HOOMAN);
+      armDesired = (Constants.ArmConstants.HOOMAN-armOffset);
    
 
   }
 
   public void scoreLowCone() {
-    sideElevator.elevator(Constants.SideElevatorConstants.MAX_DOWN+sideElvator);// go to set offset based off vision
-    elevator.elevator(Constants.ElevatorConstants.LOW_CONE);
-    arm.setMotionMagic(Constants.ArmConstants.LOW_SCORE_CONE + armOffset);
+    sideElevatorDesired  = (Constants.SideElevatorConstants.MAX_DOWN);// go to set offset based off vision
+    elevatorDesired = (Constants.ElevatorConstants.LOW_CONE);
+    armDesired = (Constants.ArmConstants.LOW_SCORE_CONE - armOffset);
 
   }
  
 
   public void scoreMidCone() {
-    sideElevator.elevator(Constants.SideElevatorConstants.MAX_DOWN+sideElvator);// go to set offset based off vision
-    elevator.elevator(Constants.ElevatorConstants.MID_CONE);
-    arm.setMotionMagic(Constants.ArmConstants.MIDDLE_CONE + armOffset);
+    sideElevatorDesired  = (Constants.SideElevatorConstants.MAX_DOWN);// go to set offset based off vision
+    elevatorDesired = (Constants.ElevatorConstants.MID_CONE);
+    armDesired = (Constants.ArmConstants.MIDDLE_CONE - armOffset);
 
   }
 
   // (((vision.range)/12)-3)*(2048/Constants.sideelevatorgearratio)*Constants.sideelevatorrotationsperfoot
   public void scoreHighCone() {
-    sideElevator.elevator(Constants.SideElevatorConstants.HIGH_CONE+sideElvator);// goto set offset based off vision
-    elevator.elevator(Constants.ElevatorConstants.HIGH_CONE);
+    sideElevatorDesired  = (Constants.SideElevatorConstants.HIGH_CONE);// goto set offset based off vision
+    elevatorDesired = (Constants.ElevatorConstants.HIGH_CONE);
 
-    arm.setMotionMagic(Constants.ArmConstants.HIGH_CONE + armOffset);
+    armDesired = (Constants.ArmConstants.HIGH_CONE - armOffset);
 
   }
   public void scoreLowCube() {
-    sideElevator.elevator(Constants.SideElevatorConstants.MAX_DOWN+sideElvator);// go to set offset based off vision
-    elevator.elevator(Constants.ElevatorConstants.LOW_CUBE);
-    arm.setMotionMagic(Constants.ArmConstants.LOW_SCORE_CUBE + armOffset);
+    sideElevatorDesired  = (Constants.SideElevatorConstants.MAX_DOWN);// go to set offset based off vision
+    elevatorDesired = (Constants.ElevatorConstants.LOW_CUBE);
+    armDesired = (Constants.ArmConstants.LOW_SCORE_CUBE - armOffset);
 
   }
  
 
   public void scoreMidCube() {
-    sideElevator.elevator(Constants.SideElevatorConstants.MAX_DOWN+sideElvator);// go to set offset based off vision
-    elevator.elevator(Constants.ElevatorConstants.MID_CUBE);
-    arm.setMotionMagic(Constants.ArmConstants.MIDDLE_CUBE + armOffset);
+    sideElevatorDesired  = (Constants.SideElevatorConstants.MAX_DOWN);// go to set offset based off vision
+    elevatorDesired = (Constants.ElevatorConstants.MID_CUBE);
+    armDesired = (Constants.ArmConstants.MIDDLE_CUBE - armOffset);
 
   }
 
   // (((vision.range)/12)-3)*(2048/Constants.sideelevatorgearratio)*Constants.sideelevatorrotationsperfoot
   public void scoreHighCube() {
-    sideElevator.elevator(Constants.SideElevatorConstants.HIGH_CUBE+sideElvator);// goto set offset based off vision
-    elevator.elevator(Constants.ElevatorConstants.HIGH_CUBE);
+    sideElevatorDesired  = (Constants.SideElevatorConstants.HIGH_CUBE);// goto set offset based off vision
+    elevatorDesired = (Constants.ElevatorConstants.HIGH_CUBE);
 
-    arm.setMotionMagic(Constants.ArmConstants.HIGH_CUBE + armOffset);
+    armDesired = (Constants.ArmConstants.HIGH_CUBE - armOffset);
 
   }
 
   public void pickup() {
 
-    sideElevator.elevator(Constants.SideElevatorConstants.PICKUP+sideElvator);
-    elevator.elevator(Constants.ElevatorConstants.PICKUP - elevatorOffset);
-    arm.setMotionMagic(Constants.ArmConstants.PICKUP);
+    sideElevatorDesired  = (Constants.SideElevatorConstants.PICKUP);
+    elevatorDesired = (Constants.ElevatorConstants.PICKUP - elevatorOffset);
+    armDesired = (Constants.ArmConstants.PICKUP);
 
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   public void zero() {
 
-    sideElevator.elevator(Constants.SideElevatorConstants.ZERO+sideElvator);
-    elevator.elevator(Constants.ElevatorConstants.ZERO);
-         if (elevator.isFinishedup(60000)){
-    arm.setMotionMagic(Constants.ArmConstants.ZERO+500);
-         }
-         armOffset=0;
-         sideElvator = 0;
+    sideElevatorDesired  = (Constants.SideElevatorConstants.ZERO);
+    elevatorDesired = (Constants.ElevatorConstants.ZERO);
+    armDesired = (Constants.ArmConstants.ZERO+500);
+
     // Use addRequirements() here to declare subsystem dependencies.
   }
 }
