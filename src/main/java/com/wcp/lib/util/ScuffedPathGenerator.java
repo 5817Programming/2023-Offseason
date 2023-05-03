516df62989de488e008418989dc947df503e6fc0
sussyBalls <sangerroboticsprogramming@gmail.com>
2023-05-02 17:59:01 -0700

    Update

--- src/main/java/com/wcp/lib/util/ScuffedPathGenerator.java
+++ src/main/java/com/wcp/lib/util/ScuffedPathGenerator.java
@@ -34,7 +34,7 @@ import org.littletonrobotics.junction.Logger;
 /** Custom PathPlanner version of SwerveControllerCommand */
 public class ScuffedPathGenerator extends SubsystemBase {
   boolean noColosions;
-  List<List<Translation2d>> objectContraints = new ArrayList();
+  List<Line2d> objectContraints = new ArrayList();
 
   private static ScuffedPathGenerator instance = null;
   public static ScuffedPathGenerator getInstance() {// if doesnt have an instance will make a new one
@@ -43,7 +43,7 @@ public class ScuffedPathGenerator extends SubsystemBase {
     return instance;
 }
 
-public static PathPlannerTrajectory generateAvoidedPath
+public static PathPlannerTrajectory generateAvoidedPath(
       PathConstraints constraints,
       Translation2d point1,
       Translation2d point2,
@@ -53,23 +53,20 @@ public static PathPlannerTrajectory generateAvoidedPath
     pointsList.add(point2);
     pointsList.addAll(List.of(points));
     for(int i = 0; i < pointsList.size()-1; i ++){
-        Line2d testLine = new Line2d(pointsList.get(i), pointsList.get(i+1));6780¿89''''''''''''''''''''''''''''''''''''''''''''''
+        Line2d testLine = new Line2d(pointsList.get(i), pointsList.get(i+1));
 
     }
     return generatePath(constraints, pointsList);
   }
 
-public static void insertObject(Line2d line1, Line2d line2, Line2d... lines){
+public void insertObject(Line2d line1, Line2d line2, Line2d... lines){
   objectContraints.add(line1);
   objectContraints.add(line2);
-  objectContraints.add(List.of(points));
+  objectContraints.addAll(List.of(lines));
 }
 
-public static void removeObject(){
+public void removeObject(){
   objectContraints.clear();
-}
-public static void removeObject(){
-  
 }
 
 
