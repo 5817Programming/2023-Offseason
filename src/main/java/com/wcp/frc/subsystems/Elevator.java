39049549ea7601089d8d2d337d51bb7cfe2e30b3
Your Name <sangerroboticsprogramming@gmail.com>
2023-05-07 14:38:02 -0700

    periodicIO

--- src/main/java/com/wcp/frc/subsystems/Elevator.java
+++ src/main/java/com/wcp/frc/subsystems/Elevator.java
@@ -16,7 +16,8 @@ import com.wcp.frc.Constants;
 import com.wcp.frc.Ports;
 
 
-public class Elevator extends SubsystemBase {
+public class Elevator extends Subsystem {
+	PeriodicIO mPeriodicIO = new PeriodicIO();
 	/* Setpoints */
 	double mTargetMin = 800;//500
 	double mTargetMax = 78000;//78000
@@ -104,31 +105,53 @@ public class Elevator extends SubsystemBase {
 }
   
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+
 
   public void elevator( double targetPos){
-    mLeftElevator.set(ControlMode.MotionMagic, targetPos);
+	mPeriodicIO.driveControlMode = ControlMode.MotionMagic;
+	mPeriodicIO.driveDemand = targetPos;
 
   }
- public boolean isFinishedup (double setpoint){
-	return mLeftElevator.getSelectedSensorPosition()<setpoint;
 
- }
- public boolean isFinisheddown (double setpoint){
-	return mLeftElevator.getSelectedSensorPosition()>setpoint;
-
- }
   public void my_PercentOutput( double speed){
 	
-	mLeftElevator.set(ControlMode.PercentOutput, speed);
-  
+	mPeriodicIO.driveControlMode = ControlMode.PercentOutput;
+	mPeriodicIO.driveDemand = speed;  
   }
   public double getEncoder(){
 	Logger.getInstance().recordOutput("elevator", mLeftElevator.getSelectedSensorPosition());
 
 	return mLeftElevator.getSelectedSensorPosition();
   }
+
+
+@Override
+public void outputTelemetry() {
+	// TODO Auto-generated method stub
+	
+}
+
+
+@Override
+public void stop() {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void writePeriodicOutputs() {
+	mPeriodicIO.drivePosition = mLeftElevator.getSelectedSensorPosition();
+	mPeriodicIO.velocity = mLeftElevator.getSelectedSensorVelocity();
+}
+@Override
+public void readPeriodicInputs() {
+	mLeftElevator.set(mPeriodicIO.driveControlMode, mPeriodicIO.driveDemand);
+}
+public static class PeriodicIO  {
+	double drivePosition = 0;
+	double velocity = 0;
+
+	ControlMode driveControlMode = ControlMode.MotionMagic;
+	double rotationDemand = 0.0;
+	double driveDemand = 0.0;
+}
 }
